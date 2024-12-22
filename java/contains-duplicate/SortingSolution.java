e4f86dae93a1fd040ef5dd9154e5b726443fe813
Ashok Jung Bahadur <ashokjung@google.com>
2024-12-22 14:27:00 +0530

    added solution

---
+++ java/contains-duplicate/SortingSolution.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class SortingSolution {
+
+    public boolean hasduplicate(int[] nums)
+    {
+        Arrays.sort(nums);
+        for(int i =1 ; i < nums.length;i++)
+        {
+            if(nums[i]== nums[i-1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] hasDuplicateElement = new int[5];
+        hasDuplicateElement[0]=1;
+        hasDuplicateElement[1]=2;
+        hasDuplicateElement[2]=3;
+        hasDuplicateElement[3]=4;
+        hasDuplicateElement[4]=3;
+        int[] hasNoDuplicateElement = new int[] {1,2,3,4,5};
+        boolean result ;
+
+        SortingSolution solution = new SortingSolution();
+        result = solution.hasduplicate(hasDuplicateElement);
+        System.out.println("Check for duplicate element in array :  "+ result);
+        result = solution.hasduplicate(hasNoDuplicateElement);
+        System.out.println("Check for No duplicate element in array :  "+ result);
+
+    }
+    
+}
