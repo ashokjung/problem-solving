22403f066f9cbc201a5cf60d9fb6645226edb08a
Ashok Jung Bahadur <ashokjung@google.com>
2024-12-23 10:30:48 +0530

    array

--- arraysAndHashing/group-anagram/java/SortingSolution.java
+++ arraysAndHashing/group-anagram/java/SortingSolution.java
@@ -6,37 +6,34 @@ import java.util.List;
 
 
 public class SortingSolution {
-    
 
-    public List<List<String>> groupAnagram(String[] strs)
-    {
-        Map<String,List<String>> result = new HashMap<>();
-        for ( String s : strs)
-        {
-            char[] charArray = s.toCharArray();
-            Arrays.sort(charArray);
-            String sortedString = new String(charArray);
-            result.putIfAbsent(sortedString, new ArrayList<>());
-            result.get(sortedString).add(s);
 
-        }
-        
-        return new ArrayList<>(result.values());
+  public static void main(String[] args) {
+    SortingSolution solution = new SortingSolution();
+    List<List<String>> result;
+    String[] string = new String[]{"act", "pots", "tops", "cat", "stop", "hat"};
+    String[] string1 = new String[]{"x"};
+    String[] string2 = new String[]{""};
+    result = solution.groupAnagram(string);
+    System.out.println(result);
+    result = solution.groupAnagram(string1);
+    System.out.println(result);
+    result = solution.groupAnagram(string2);
+    System.out.println(string2);
+  }
+
+  public List<List<String>> groupAnagram(String[] strs) {
+    Map<String, List<String>> result = new HashMap<>();
+    for (String s : strs) {
+      char[] charArray = s.toCharArray();
+      Arrays.sort(charArray);
+      String sortedString = new String(charArray);
+      result.putIfAbsent(sortedString, new ArrayList<>());
+      result.get(sortedString).add(s);
+
     }
 
-    public static void main(String[] args)
-    {
-        SortingSolution solution = new SortingSolution();
-        List<List<String>> result;
-        String[] string = new String[]{"act","pots","tops","cat","stop","hat"};
-        String[] string1 = new String[]{"x"};
-        String[] string2 = new String[]{""};
-        result= solution.groupAnagram(string);
-        System.out.println(result);
-        result = solution.groupAnagram(string1);
-        System.out.println(result);
-        result = solution.groupAnagram(string2);
-        System.out.println(string2);
-    }
+    return new ArrayList<>(result.values());
+  }
 
 }
