6d0cce40732b2622d6b8ebb7dc9d4e8392294533
Ashok Jung Bahadur <ashokjung@gmail.com>
2025-01-02 22:13:46 +0530

    added valid sudoku

--- arraysAndHashing/valid-sudoku/python/ValidSudoku.py
+++ arraysAndHashing/valid-sudoku/python/ValidSudoku.py
@@ -1,53 +1,66 @@
-def is_valid_sudoku(board):
-    def is_valid_unit(unit):
+class SudokuValidator:
+    def __init__(self, board):
+        self.board = board
+
+    def is_valid_unit(self, unit):
         unit = [i for i in unit if i != '.']
         return len(unit) == len(set(unit))
 
-    def is_valid_row(board):
-        for row in board:
-            if not is_valid_unit(row):
+    def is_valid_row(self):
+        for row in self.board:
+            if not self.is_valid_unit(row):
                 return False
         return True
 
-    def is_valid_col(board):
-        for col in zip(*board):
-            if not is_valid_unit(col):
+    def is_valid_col(self):
+        for col in zip(*self.board):
+            if not self.is_valid_unit(col):
                 return False
         return True
 
-    def is_valid_square(board):
+    def is_valid_square(self):
         for i in (0, 3, 6):
             for j in (0, 3, 6):
-                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
-                if not is_valid_unit(square):
+                square = [self.board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.is_valid_unit(square):
                     return False
         return True
 
-    return is_valid_row(board) and is_valid_col(board) and is_valid_square(board)
+    def is_valid_sudoku(self):
+        return self.is_valid_row() and self.is_valid_col() and self.is_valid_square()
 
-board = [
-    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-    [".", "9", "8", ".", ".", ".", ".", "6", "."],
-    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-    [".", "6", ".", ".", ".", ".", "2", "8", "."],
-    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-]
 
-board1 = [
-    ["1","2",".",".","3",".",".",".","."],
-    ["4",".",".","5",".",".",".",".","."],
-    [".","9","1",".",".",".",".",".","3"],
-    ["5",".",".",".","6",".",".",".","4"],
-    [".",".",".","8",".","3",".",".","5"],
-    ["7",".",".",".","2",".",".",".","6"],
-    [".",".",".",".",".",".","2",".","."],
-    [".",".",".","4","1","9",".",".","8"],
-    [".",".",".",".","8",".",".","7","9"]
-]
+def main():
+    board = [
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    ]
 
-print(is_valid_sudoku(board)) 
-print(is_valid_sudoku(board1)) 
+    board1 = [
+        ["1","2",".",".","3",".",".",".","."],
+        ["4",".",".","5",".",".",".",".","."],
+        [".","9","1",".",".",".",".",".","3"],
+        ["5",".",".",".","6",".",".",".","4"],
+        [".",".",".","8",".","3",".",".","5"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".",".",".",".",".",".","2",".","."],
+        [".",".",".","4","1","9",".",".","8"],
+        [".",".",".",".","8",".",".","7","9"]
+    ]
+
+    validator1 = SudokuValidator(board)
+    validator2 = SudokuValidator(board1)
+
+    print(validator1.is_valid_sudoku()) 
+    print(validator2.is_valid_sudoku()) 
+
+
+if __name__ == "__main__":
+    main()
