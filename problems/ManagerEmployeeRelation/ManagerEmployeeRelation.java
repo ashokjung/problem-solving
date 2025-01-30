19b62c710735a7b4913478f8be5a04629d3cfd92
Ashok Jung Bahadur <ashokjung@google.com>
2025-01-30 21:20:53 +0530

    update ManagerEmployeeRelation.java

--- problems/ManagerEmployeeRelation/ManagerEmployeeRelation.java
+++ problems/ManagerEmployeeRelation/ManagerEmployeeRelation.java
@@ -1,6 +1,8 @@
 package problems.ManagerEmployeeRelation;
 import java.util.ArrayList;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ManagerEmployeeRelation {
     int employeeId;
